1b985ba0182fc134e5460ca1ff8b63b024ddf02c
Delyan Boychev <delyan.boychev05@gmail.com>
2022-04-22 09:15:20 +0300

    PCA and K-means working

---
+++ KMeansClusteringPCA/computeCentroids.m
@@ -0,0 +1,46 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+for i=1:K,
+    N = find(idx == i);
+    numP = sum(idx == i);
+    vals = zeros(numP, size(X, 2));
+    for j=1:numP,
+        vals(j, :) = X(N(j), :);
+    end
+    centroids(i, :) = (1/numP)*sum(vals);
+end
+
+
+
+
+% =============================================================
+
+
+end
+
